fcd6e54649dc7c8c1d83751a2fb154b7e43609d1
Bartosz Czechowski <czechowski.bartek123@gmail.com>
2024-06-10 23:40:20 +0200

    first cleanup

--- crc.asm
+++ crc.asm
@@ -15,6 +15,7 @@ LSEEK_CUR equ 1
 
 BUFFER_SIZE equ 8
 SHIFT_FROM_THE_YOUNGEST_TO_THE_OLDEST_EIGHT_BITS equ 56
+NEWLINE equ 10
 
 section .data
     arg1_msg db 'File: ', 0
@@ -59,66 +60,26 @@ section .text
 %endmacro
 
 _start:
-printing_parameters:
-    ; Get command line arguments
+processing_parameters:
     mov rdi, [rsp]          ; argc (number of arguments)
-    cmp rdi, 3              ; We need exactly 2 arguments, so 3 in total with program name
-    jne _error              ; If not, jump to error handler
+    cmp rdi, 3              ; 2 args + 1 program name
+    jne _not_closing_error              
 
-    ; Print first argument message
-    PRINT_STRING arg1_msg
-
-    ; Print first argument
-    mov rsi, [rsp + 16]     ; pointer to first argument (argv[1])
-    call print_string       ; print the first argument
-
-    ; Print newline
-    PRINT_STRING newline
-
-    ; Print second argument message
-    PRINT_STRING arg2_msg
-
-    ; Print second argument
-    mov rsi, [rsp + 24]     ; pointer to second argument (argv[2])
-    call print_string       ; print the second argument
-
-    PRINT_STRING newline
-
-    ;mov rdx, [rsp + 24]
     call parse_poly
-    ;mov rax, [poly]
-    ;print "poly parse: ", rax
-
-    ; Print newline
-    PRINT_STRING newline
-
 opening_file:
-    mov rax, SYS_OPEN                         ; sys_open
-    mov rdi, [rsp + 16]             ; Pointer to file name
-    mov rsi, READ_ONLY                        ; Flags: O_RDONLY (0)
-    mov rdx, MODE                          ; Mode (ignored because we are not creating a file)
+    mov rax, SYS_OPEN
+    mov rdi, [rsp + 16] ; file path
+    mov rsi, READ_ONLY                        
+    mov rdx, MODE ; ignored
     syscall
 
     cmp rax, 0
-    jl _error                      ; If rax < 0, jump to error handling
+    jl _not_closing_error
 
-    ; Store the file descriptor
     mov [fd], rax
 
 processing_file:
-
-	; only ret here once its over
-    ; call fetching_test
-    ;mov qword [poly], 0b1
-    ;shl qword [poly], 63
-    ;mov qword [poly], 0b11
-    ;shl qword [poly], 61 ; correctry came out as 4 = 0b0100
-    ; mov qword [poly], 0b11010101
-    ;shl qword [poly], 56 ; correctly passes with 57 = 0101 0111
-    mov rax, [poly]
-    print "poly: ", rax
     call filling_initial_buffer
-    print "brat: ", r9
     call print_poly_result
 
 closing_file:
@@ -133,6 +94,10 @@ closing_file:
     syscall
 
 _error:
+    mov rax, SYS_CLOSE
+    mov rdi, [fd]
+    syscall
+_not_closing_error:
     ; Handle error (print error message and exit with code 1)
     PRINT_STRING error_msg
 
@@ -164,60 +129,45 @@ read_data_len:
     mov rsi, data_len
     mov rdx, 2 ; length is a word - 2 bytes
     syscall
-    ; TODO add checking for fails
-    ; now in word [data_len] we have the number of bytes in curr segment
 
     cmp rax, 0
     jl _error
 
+    ; if the segment has 0 data we immedietaly seek the next one
     xor rax, rax
     mov ax, word [data_len]
     cmp ax, 0
     je reading_offset
-    ; print "length: ", rax
 
     xor eax, eax
     mov word [data_counter], ax ; bytes read in curr segment - 0
 
-    cmp word [data_len], 0
-    jne .end
-    mov rax, LAST_BYTE ; if no bytes in current segment we want to return last byte
-
-.end:
+    ; !!! what the hell was this code??
     ret ; we return 0 because there are bytes in current segment
 
 ; fetches next byte to a small buffer
 ; if it was the last byte then rax is set to -1 = LAST_BYTE
 ; otherwise to 0
-; TODO change logic to allow zero length segments
-; read data len should set LAST_BYTE if necessary
-; and condition should be moved to the top
 fetch_next_byte:
     ; small_buffer := curent byte
     mov rax, SYS_READ
     mov rdi, [fd]
-    mov rsi, small_buffer ; TODO push from the back 
+    mov rsi, small_buffer ; 
     mov rdx, 1 ; process only one byte at once - endian order...
     syscall
 
     cmp rax, 0
     jl _error
 
-    ; print buffer
-    xor rax, rax
-    mov al , byte [small_buffer]
-    ; print "Read: ", rax
-
     mov ax, word [data_counter]
     inc ax
-    mov word [data_counter], ax
+    mov word [data_counter], ax ; data read in current segment increases
     cmp ax, word [data_len]
-    je reading_offset
-    xor eax, eax ; there is more data in the current segment
+    je reading_offset ; if read all data in current segment proceed to the next
+    xor eax, eax ; otherwise there is more data in the current segment
     ret ; so we return 0
 
 reading_offset:
-    ; TODO optimize syscall args
     mov rax, SYS_READ
     mov rdi, [fd]
     mov rsi, segment_offset
@@ -228,7 +178,6 @@ reading_offset:
     jl _error
 
     movsxd rsi, dword [segment_offset]
-    ; print "offset: ", rsi
 
     ; rdx := total length of curr segment
     xor edx, edx ; rdx := 0
@@ -238,7 +187,7 @@ reading_offset:
     add rsi, rdx
     cmp rsi, 0
     jne .moving_to_next_segment
-    mov rax, LAST_BYTE
+    mov rax, LAST_BYTE ; no more data
     ret ; return LAST_BYTE
 .moving_to_next_segment:
     mov rax, SYS_LSEEK
@@ -253,20 +202,7 @@ reading_offset:
     jmp read_data_len
     
 
-fetching_test:
-    call read_data_len
-.crawling_condition:
-    cmp rax, LAST_BYTE
-    je .after_file
-.crawling_loop_body:
-    call fetch_next_byte
-    mov dl, byte [small_buffer]
-    print "read: ", rdx
-    jmp .crawling_condition
-.after_file:
-    ret
 
-; RDX MODIFIED!!!!!!!!
 filling_initial_buffer:
     call read_data_len ; zeros rax
     ; r8 <- [7, 0]
@@ -281,13 +217,12 @@ filling_initial_buffer:
     call fetch_next_byte ; rax has to be unchanged until the xor loop
     mov dl, byte [small_buffer] ; RDX!!
     mov byte [buffer + r8], dl
-    inc byte [bytes_in_buffer] ; adding a byte
+    inc byte [bytes_in_buffer] ; new byte in buffer
     dec r8
     jmp .condition
 .after_loop:
     jmp xoring_loop
 
-; rbx!!!
 xoring_loop:
     mov r9, qword [buffer]
 .loop_unprocessed_bits_condition:
@@ -319,61 +254,53 @@ xoring_loop:
 .can_proceed_with_xor:
     xor r9, qword [poly]
 .inner_loop_lower_body:
-    ;print "content of r9: ", r9
     inc r8
     jmp .inner_loop_condition
 .after_loop:
-    print "result in hex: ", r9
     ret
 
 
 parse_poly:
-    mov rdx, [rsp + 32]               ; rcx = pointer to the string
-    mov al, [rdx]              ; Load first character of the string into al
-    test al, al                ; Check if it's the null terminator
-    jz _error                  ; If it is, jump to _error
+    mov rdx, [rsp + 32] ; polynomial string ptr
+    mov al, [rdx]              
+    test al, al ; if first character null raise error
+    jz _not_closing_error
 
-    ; Find the length of the string
+    ; find the length of the string
     xor ecx, ecx
-find_length:
-    mov al, [rdx + rcx]        ; Load current character into al
-    test al, al                ; Check if it's the null terminator
-    jz process_string          ; If it is, start processing the string
-    inc rcx                    ; Increment counter
-    jmp find_length            ; Loop back to find length
-
-process_string:
+.find_length:
+    mov al, [rdx + rcx]        
+    test al, al                
+    jz .process_string ; while not null
+    inc rcx           ; i++
+    jmp .find_length          
+.process_string:
     mov [poly_length], cl
-    dec rcx                    ; rdi now points to the last character of the string
+    cmp rcx, 64
+    ja _not_closing_error ; polynomial too long
+    dec rcx ; rcx now points to the last character of the string
+.process_char:
+    mov al, [rdx + rcx]        
+    cmp al, '0'                
+    je .store_zero_bit          
 
-process_char:
-    mov al, [rdx + rcx]        ; Load the current character into al
-    cmp al, '0'                ; Compare the character with '0'
-    je store_zero_bit          ; If it's '0', store zero bit
+    cmp al, '1'
+    je .store_one_bit
 
-    cmp al, '1'                ; Compare the character with '1'
-    je store_one_bit           ; If it's '1', store one bit
-
-    jmp _error                 ; If it's neither '0' nor '1', jump to _error
-
-
-store_one_bit:
-    ; Shift poly left by 1 bit and set the LSB
+    jmp _not_closing_error ; wrong character
+.store_one_bit:
     mov rax, 1
     shl rax, 63
-    shr rax, cl
+    shr rax, cl ; turns on the rcx-th (cl-th since its <=64) bit
     or qword [poly], rax
-    jmp check_done
-
-store_zero_bit:
-    ; do nothign
-
-check_done:
-    dec rcx                    ; Move to the previous character
-    js done                    ; If we've processed all characters, we're done
-    jmp process_char           ; Loop back to process the next character
-
-done: 
+    jmp .check_done
+.store_zero_bit:
+    ; do nothing
+.check_done:
+    dec rcx
+    js .done ; it was the last char
+    jmp .process_char ; store prev char
+.done: 
     ret
     ; TODO: parse polynomial
     ; TODO: print result
@@ -383,34 +310,29 @@ done:
 ; assumes result in r9 
 ; will go through the input string and now to end from there
 print_poly_result:
-    mov rcx, [poly_length]                   ; rcx = poly_length (number of bits to write)
-    lea rsi, [buffer]              ; rsi = address of buffer
-    xor rdi, rdi                   ; rdi = index in buffer
+    mov rcx, [poly_length] ; rcx = poly_length (number of bits to write)
+    lea rsi, [print_buffer] ; rsi = address of buffer for printing result
+    xor rdi, rdi ;  rdi = index in buffer
 
 write_bits:
-    ; Get the most significant bit of r9
     mov rbx, r9
-    shr rbx, 63                    ; Shift right to get the most significant bit
-    add rbx, '0'                   ; Convert bit to ASCII ('0' or '1')
-    mov byte [rsi + rdi], bl            ; Store the character in the buffer
-    inc rdi                        ; Move to the next buffer position
+    shr rbx, 63                    ; rbx := oldest bit in r9
+    add rbx, '0'                   ; convert bit to ASCII ('0' or '1')
+    mov byte [rsi + rdi], bl       ; store char
+    inc rdi                      
 
     ; Shift r9 left by 1 to process the next bit
-    shl r9, 1
+    shl r9, 1 ; first bit stored so we pretend the polynomial is shorter
+    dec rcx ; and repeat
+    jnz write_bits
 
-    ; Decrement the bit counter
-    dec rcx
-    jnz write_bits                 ; Repeat until all bits are processed
-
-    mov byte [rsi + rdi], 10
+    mov byte [rsi + rdi], NEWLINE 
     inc rdi
-    mov byte [rsi + rdi], 0
+    ; following byte 0 by default
 
-
-    ; Write the buffer to stdout
-    mov rax, SYS_WRITE                     ; syscall: write
-    mov rdx, rdi                   ; number of bytes to write
-    mov rdi, STDOUT                     ; file descriptor: stdout
-    lea rsi, [buffer]              ; buffer to write
+    mov rax, SYS_WRITE                     
+    mov rdx, rdi ; unusual order due to somwhat poor choice of registers...
+    mov rdi, STDOUT                     
+    lea rsi, [print_buffer]              
     syscall
     ret
